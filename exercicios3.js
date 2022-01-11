d26ce84530fb64d8dd10e1f32e4173d29d4d0978
NullBeta001 <fabiooli221297@gmail.com>
2022-01-11 12:12:09 -0300

    Adição do exercicio 3

---
+++ exercicios3.js
@@ -0,0 +1,8 @@
+let listaDeEstudantes = ["Helena", "Chico", "Mário", "Pedro"];
+let quantidadeDeEstudantes = listaDeEstudantes.length;
+if (quantidadeDeEstudantes < 4) {
+    listaDeEstudantes.push("Paulo")
+} else {
+    console.log("Quatidade máxima atingida")
+}
+console.log(listaDeEstudantes)
